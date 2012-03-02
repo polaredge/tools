6ca450a9476dcd69dd226912cd0cfcb150a7c4d1
Manoj Kumar <mkumar@enovafinancial.com>
2012-03-01 18:30:28 -0600

    Major changes

--- scripts/dbutil.sh
+++ scripts/dbutil.sh
@@ -13,12 +13,20 @@ echo "OR enter anything else to EXIT"
 
 read action
 
+# DB BACKUP
 if [ $action -eq 1 ]; then
-
+# BEFORE We delete OLD Backups, make sure orignal DB exists to make Backups
+SOURCE_DB=$(psql -U postgres -l | grep -w $DB)
+	if [ -z "$SOURCE_DB" ]; then
+           echo "Source DB $DB does not exist. Current DB LIST:"
+	   psql -U postgres -l
+           exit 0
+	fi
 # Delete OLD Back up DBs
 psql -U postgres -l | grep -w $DB_BACKUP | awk '{ system("dropdb -U postgres " $1)}'
 psql -U postgres -l | grep -w $DB_FRAUD_BACKUP | awk '{ system("dropdb -U postgres " $1)}'
 
+# BACKUP NOW
 createdb -U postgres -T $DB  $DB_BACKUP
 createdb -U postgres -T $DB_FRAUD  $DB_FRAUD_BACKUP
 
@@ -27,12 +35,21 @@ echo "Backed UP $DB_FRAUD to $DB_FRAUD_BACKUP"
 exit 0
 fi
 
+# DB RESTORE
 if [ $action -eq 2 ]; then
+# Before we delete DBs, make sure backup DB exists
+SOURCE_DB=$(psql -U postgres -l | grep -w $DB_BACKUP)
+	if [ -z "$SOURCE_DB" ]; then
+           echo "Backup DB $DB_BACKUP does not exist. Current DB List"
+	   psql -U postgres -l
+           exit 0
+	fi
 
 # Delete current DB
 psql -U postgres -l | grep -w $DB | awk '{ system("dropdb -U postgres " $1)}'
 psql -U postgres -l | grep -w $DB_FRAUD | awk '{ system("dropdb -U postgres " $1)}'
 
+# RESTORE NOW
 createdb -U postgres -T $DB_BACKUP  $DB
 createdb -U postgres -T $DB_FRAUD_BACKUP  $DB_FRAUD
 
